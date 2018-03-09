d7a9b73eb247ce4c062c853468bef1aa6b9e8354
jammer312 <remordemef312@yandex.ru>
2018-03-10 01:48:18 +0300

    probably fixed crash notification

--- main.go
+++ main.go
@@ -9,18 +9,20 @@ import (
 )
 
 func main() {
-	Dopen()              //start discord
+	Dopen() //start discord
+	defer func() {
+		if r := recover(); r != nil {
+			Discord_message_send("bot_status", "STATUS UPDATE", "", "CRASH")
+			time.Sleep(time.Duration(5) * time.Second)
+			panic(r)
+		}
+	}()
 	srv := Http_server() //start web server
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc     //wait for SIGINT or kinda it
 	Dclose() //stop discord
 	//graceful shutdown for web server
-	if r := recover(); r != nil {
-		Discord_message_send("bot_status", "STATUS UPDATE", "", "CRASH")
-		time.Sleep(time.Duration(5) * time.Second)
-		panic(r)
-	}
 	if err := srv.Shutdown(nil); err != nil {
 		log.Fatal("Failed to shutdown webserver: ", err)
 	}
