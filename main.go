c47f25f6dd1fad2f281c03bc7799ff93ee2b5e58
jammer312 <remordemef312@yandex.ru>
2018-03-10 01:54:20 +0300

    removed non-working crash notification

--- main.go
+++ main.go
@@ -5,18 +5,10 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 )
 
 func main() {
-	Dopen() //start discord
-	defer func() {
-		if r := recover(); r != nil {
-			Discord_message_send("bot_status", "STATUS UPDATE", "", "CRASH")
-			time.Sleep(time.Duration(5) * time.Second)
-			panic(r)
-		}
-	}()
+	Dopen()              //start discord
 	srv := Http_server() //start web server
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
