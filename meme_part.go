e30f2ab7bf6f6a7d5935dd1eacb4c8ced0660c4f
jammer312 <remordemef312@yandex.ru>
2018-05-23 22:34:24 +0300

    autoplay

--- meme_part.go
+++ meme_part.go
@@ -19,7 +19,7 @@ const (
 	<title>regular dank meme</title>
 	%v
 	</head>
-	<body><iframe width="854" height="480" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></body>
+	<body><iframe width="854" height="480" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></body>
 	</html>`
 )
 
