29470529ab64bdea5f86604d7be65fe0f8a49a1f
jammer312 <remordemef312@yandex.ru>
2018-05-23 22:01:02 +0300

    that didn't work as expected

--- meme_part.go
+++ meme_part.go
@@ -31,7 +31,11 @@ const (
 	</html>`
 )
 
-var meme_stealthmode bool
+var meme_stealthmode, meme_first_init_done bool
+var (
+	meme_meta    string
+	meme_content string
+)
 
 func meme_init() bool {
 	defer logging_recover("epic meme fail")
@@ -39,16 +43,19 @@ func meme_init() bool {
 	desc := get_config_must("meme_description")
 	image := get_config_must("meme_image")
 	redirect_url := get_config_must("meme_redirect_url")
-	meta := fmt.Sprintf(meta_head, title, desc, image)
-	meme := fmt.Sprintf(redirect_head, redirect_url)
-	http.HandleFunc("/meme", func(w http.ResponseWriter, r *http.Request) {
-		if meme_stealthmode {
-			meme_stealthmode = false
-			fmt.Fprintf(w, docwrapper, meta)
-		} else {
-			fmt.Fprintf(w, docwrapper, meme)
-		}
-	})
+	meme_meta = fmt.Sprintf(meta_head, title, desc, image)
+	meme_content = fmt.Sprintf(redirect_head, redirect_url)
+	if !meme_first_init_done {
+		meme_first_init_done = true
+		http.HandleFunc("/meme", func(w http.ResponseWriter, r *http.Request) {
+			if meme_stealthmode {
+				meme_stealthmode = false
+				fmt.Fprintf(w, docwrapper, meme_meta)
+			} else {
+				fmt.Fprintf(w, docwrapper, meme_content)
+			}
+		})
+	}
 	return true
 }
 
